212412111936c04bfada1160fe94ca4d5d1327d4
nadiaifti <nadiaifti24@gmail.com>
2021-10-25 15:28:22 +0700

    Biodata

---
+++ minggu4/Project 1/main.cpp
@@ -0,0 +1,30 @@
+//JUDUL: Praktik 1
+/*
+inikomentar, tidak terbaca oleh program
+Nama            = Nadia Ifti Khani
+Tempat, tanggal = Pekalongan, 24 April 2003
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!\n";
+    // \n untuk enter, \t untuk tab
+    cout << "Dasar \tPemrograman" << endl; //fungsi endl untuk enter
+    cout << "Hari \t\t= Kamis\n";
+    cout << "Tanggal \t= 7 \Oktober\ 2021 \\ Kamis Legi" <<endl;
+    cout << "Apa itu \'teknologi\'?" << endl;
+    cout << "\t***\n***\t\t***\n\t***" << endl;
+    cout << "Hello Dunia!" << endl;
+    // \" untuk menulis " di console
+    // \\ untuk menulis \
+
+    cout << "000\t000\n000" <<endl;
+    // \t = 4 spasi
+
+
+    return 0;
+}
+
